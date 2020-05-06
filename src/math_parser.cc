96c92d8d9e99cf82fb22d3ac925a700fdcbfd3ee
He Zhang <zhanghe1979@gmail.com>
2020-05-06 14:02:28 -0400

    Fixing math_parser bug: functions in capital fonts not recognized.

--- src/math_parser.cc
+++ src/math_parser.cc
@@ -76,6 +76,64 @@ void ListVar(muParserHandle_t a_hParser)
     }
 }
 
+muFloat_t Sin(muFloat_t v){return sin(v);}
+muFloat_t Cos(muFloat_t v){return cos(v);}
+muFloat_t Tan(muFloat_t v){return tan(v);}
+muFloat_t Asin(muFloat_t v){return asin(v);}
+muFloat_t Acos(muFloat_t v){return acos(v);}
+muFloat_t Atan(muFloat_t v){return atan(v);}
+muFloat_t Sinh(muFloat_t v){return sinh(v);}
+muFloat_t Cosh(muFloat_t v){return cosh(v);}
+muFloat_t Tanh(muFloat_t v){return tanh(v);}
+muFloat_t Asinh(muFloat_t v){return asinh(v);}
+muFloat_t Acosh(muFloat_t v){return acosh(v);}
+muFloat_t Atanh(muFloat_t v){return atanh(v);}
+muFloat_t Log(muFloat_t v){return log(v);}
+muFloat_t Log10(muFloat_t v){return log10(v);}
+muFloat_t Log2(muFloat_t v){return log2(v);}
+muFloat_t Exp(muFloat_t v){return exp(v);}
+muFloat_t Sqrt(muFloat_t v){return sqrt(v);}
+muFloat_t Sign(muFloat_t v){if(v>0) return 1; if(v<0) return -1; return 0;}
+muFloat_t Rint(muFloat_t v){return rint(v);}
+muFloat_t Floor(muFloat_t v){return floor(v);}
+muFloat_t Ceil(muFloat_t v){return ceil(v);}
+muFloat_t Abs(muFloat_t v){return fabs(v);}
+
+muFloat_t Sum(const muFloat_t *a_afArg, int a_iArgc){
+    muFloat_t fRes = 0;
+    int i = 0;
+    for (i = 0; i < a_iArgc; ++i)
+        fRes += a_afArg[i];
+    return fRes;
+}
+
+muFloat_t Avg(const muFloat_t *a_afArg, int a_iArgc){
+    muFloat_t fRes = 0;
+    int i = 0;
+    for (i = 0; i < a_iArgc; ++i)
+        fRes += a_afArg[i];
+    fRes /= a_iArgc;
+    return fRes;
+}
+
+muFloat_t Max(const muFloat_t *a_afArg, int a_iArgc){
+    muFloat_t fRes = a_afArg[0];
+    int i = 1;
+    for (i = 1; i < a_iArgc; ++i)
+        if(a_afArg[i]>fRes)
+            fRes = a_afArg[i];
+    return fRes;
+}
+
+muFloat_t Min(const muFloat_t *a_afArg, int a_iArgc){
+    muFloat_t fRes = a_afArg[0];
+    int i = 1;
+    for (i = 1; i < a_iArgc; ++i)
+        if(a_afArg[i]<fRes)
+            fRes = a_afArg[i];
+    return fRes;
+}
+
 //---------------------------------------------------------------------------
 void ListExprVar(muParserHandle_t a_hParser)
 {
@@ -153,12 +211,45 @@ void AddConst(muParserHandle_t hParser) {
     mupDefineConst(hParser, "K_KE", k_ke);
 }
 
+void AddFun(muParserHandle_t hParser) {
+    mupDefineFun1(hParser, _T("SIN"), Sin, 1);
+    mupDefineFun1(hParser, _T("COS"), Cos, 1);
+    mupDefineFun1(hParser, _T("TAN"), Tan, 1);
+    mupDefineFun1(hParser, _T("ASIN"), Asin, 1);
+    mupDefineFun1(hParser, _T("ACOS"), Acos, 1);
+    mupDefineFun1(hParser, _T("ATAN"), Atan, 1);
+    mupDefineFun1(hParser, _T("SINH"), Sinh, 1);
+    mupDefineFun1(hParser, _T("COSH"), Cosh, 1);
+    mupDefineFun1(hParser, _T("TANH"), Tanh, 1);
+    mupDefineFun1(hParser, _T("ASINH"), Asinh, 1);
+    mupDefineFun1(hParser, _T("ACOSH"), Acosh, 1);
+    mupDefineFun1(hParser, _T("ATANH"), Atanh, 1);
+    mupDefineFun1(hParser, _T("Ln"), Log, 1);
+    mupDefineFun1(hParser, _T("LOG"), Log, 1);
+    mupDefineFun1(hParser, _T("LOG10"), Log10, 1);
+    mupDefineFun1(hParser, _T("LOG2"), Log2, 1);
+    mupDefineFun1(hParser, _T("EXP"), Exp, 1);
+    mupDefineFun1(hParser, _T("SQRT"), Sqrt, 1);
+    mupDefineFun1(hParser, _T("RINT"), Rint, 1);
+    mupDefineFun1(hParser, _T("FLOOR"), Floor, 1);
+    mupDefineFun1(hParser, _T("CEIL"), Ceil, 1);
+    mupDefineFun1(hParser, _T("floor"), Floor, 1);
+    mupDefineFun1(hParser, _T("ceil"), Ceil, 1);
+    mupDefineFun1(hParser, _T("ABS"), Abs, 1);
+    mupDefineMultFun(hParser, _T("SUM"), Sum, 1);
+    mupDefineMultFun(hParser, _T("AVG"), Avg, 1);
+    mupDefineMultFun(hParser, _T("MEAN"), Avg, 1);
+    mupDefineMultFun(hParser, _T("MAX"), Max, 1);
+    mupDefineMultFun(hParser, _T("MIN"), Min, 1);
+}
+
 //Initialize the math parser environment
 void initialize_parser(muParserHandle_t &math_parser) {
     math_parser = mupCreate(muBASETYPE_FLOAT);              // initialize the parser
     mupSetErrorHandler(math_parser, OnError);
     mupSetVarFactory(math_parser, AddVariable, NULL);       // Set a variable factory
-    AddConst(math_parser);                                  // Add constants
+    AddConst(math_parser);
+    AddFun(math_parser);                            // Add constants
 
     //Set variables to save the calculation/simulation results.
     mupDefineVar(math_parser, "VL_EMIT_NX", &uircd.emit_nx);
